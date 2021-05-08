432740162f951b446c1acf6468314e8a8af3434d
Alex-hhernandez <82846948+Alex-hhernandez@users.noreply.github.com>
2021-05-08 15:25:39 -0600

    Add files via upload

---
+++ Ejercicio2/src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,20 @@
+package ejercicio2;
+
+/**
+ *
+ * @author Alex_Hernandez
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Proyecto_JAVA");
+        System.out.println("------------------------");
+        System.out.println("Ejercicio2");
+        System.out.println("-------------------------");
+        System.out.println("Edwin Alexander");
+    }
+    
+}
